1de3b2debe1879ac32b55db34d007483f557fe4a
kaikecesar <kaikeoliveira196@gmail.com>
2026-05-21 12:37:10 -0300

    feat(database): add user schema and migration setup
    - Updated package.json to include Drizzle ORM and PostgreSQL dependencies.
    - Created a new drizzle.config.ts for migration configuration.
    - Implemented database connection in index.ts using Drizzle ORM.
    - Added SQL migration for creating users table with necessary fields.
    - Created schema definition for users table in users.ts.
    - Added Docker Compose configuration for PostgreSQL service.
    - Commented out JWT_SECRET in env schema for future implementation.

---
+++ backend/src/database/schema/users.ts
@@ -0,0 +1,15 @@
+// Libraries
+import { pgTable, varchar, uuid, text, timestamp } from 'drizzle-orm/pg-core';
+
+export const usersTable = pgTable('users', {
+  id: uuid().primaryKey().defaultRandom(),
+  name: varchar({ length: 100 }).notNull(),
+  email: varchar({ length: 100 }).notNull().unique(),
+  passwordHash: text('password_hash').notNull(),
+  phone: varchar('phone', { length: 20 }),
+  createdAt: timestamp('created_at', { withTimezone: true })
+    .defaultNow()
+    .notNull(),
+  updatedAt: timestamp('updated_at', { withTimezone: true }),
+  deletedAt: timestamp('deleted_at', { withTimezone: true }),
+});
