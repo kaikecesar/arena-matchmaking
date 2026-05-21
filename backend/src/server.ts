859318eacf0af68a9630c011bf73ee02c05cd0b4
kaikecesar <kaikeoliveira196@gmail.com>
2026-05-21 11:01:54 -0300

    refactor: update backend configuration and server setup
    - Updated package.json to include build script using tsup and modified lint command.
    - Refactored server.ts to import environment variables and listen on a configurable port.
    - Adjusted tsconfig.json to allow importing TypeScript extensions and set noEmit to true for stricter type checking.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,11 @@
 // Libraries
 import fastify from 'fastify';
 
+// Application
+import { env } from './env/index.ts';
+
 const app = fastify();
 
-const start = async () => {
-  await app.listen({ port: 3000, host: '0.0.0.0' });
-  console.log('Server running on port 3000');
-};
-
-start().catch(console.error);
+app.listen({ host: '0.0.0.0', port: env.PORT }, () =>
+  console.log('🚀 HTTP Server Running!'),
+);
